12f5e8d0d22c593922f487e02e2b101c5a19f187
navodRathnayake <navodhosts@gmail.com>
2022-09-05 17:24:00 +0530

    added more images view

--- lib/Custom Widgets/BottomNavigationSheets/DestinationSheet.dart
+++ lib/Custom Widgets/BottomNavigationSheets/DestinationSheet.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:pilgramz/Utils/jason.dart';
 
 import '../HorizontalSpace.dart';
 import '../VerticalSpace.dart';
@@ -25,47 +27,85 @@ class _DestinationSheetState extends State<DestinationSheet> {
         ),
         child: Padding(
             padding: EdgeInsets.only(left: 18, right: 18),
-            child: ListView(
-              controller: scrollController,
-              children: [
-                VerticalSpace(height: 10),
-                Center(
-                  child: Container(
-                    width: 50,
-                    height: 5,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(10),
+            child: Column(children: [
+              VerticalSpace(height: 10),
+              Center(
+                child: Container(
+                  width: 50,
+                  height: 5,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Transform.rotate(
+                    angle: 3.1,
+                    child: Icon(Icons.format_quote),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  HorizontalSpace(width: 12),
+                  Expanded(
+                    child: Text(
+                      'Ultimate Sri Lanka Street Food Tour In Colombo With The Kindest Vendors',
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: widget.themedata.textTheme.headline4,
+                    ),
+                  ),
+                ],
+              ),
+              VerticalSpace(height: 10),
+              const Divider(),
+              VerticalSpace(height: 10),
+              Expanded(
+                child: Container(
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 8,
+                      crossAxisSpacing: 8,
+                    ),
+                    itemCount: destinationImages.length,
+                    itemBuilder: (context, index) => Container(
+                      child: Container(
+                        width: 70,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        child: CachedNetworkImage(
+                          fit: BoxFit.fill,
+                          imageUrl: destinationImages[index],
+                          placeholder: (context, url) =>
+                              CircularProgressIndicator(),
+                          errorWidget: (context, url, error) =>
+                              Icon(Icons.error),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Transform.rotate(
-                      angle: 3.1,
-                      child: Icon(Icons.format_quote),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    HorizontalSpace(width: 12),
-                    Expanded(
-                      child: Text(
-                        'Ultimate Sri Lanka Street Food Tour In Colombo With The Kindest Vendors',
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        style: widget.themedata.textTheme.headline4,
-                      ),
-                    ),
-                  ],
-                ),
-                VerticalSpace(height: 10),
-                const Divider(),
-                VerticalSpace(height: 10)
-              ],
-            )),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25))),
+                  foregroundColor:
+                      Theme.of(context).colorScheme.onSecondaryContainer,
+                  backgroundColor:
+                      Theme.of(context).colorScheme.secondaryContainer,
+                ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+                onPressed: () {},
+                child: const Text('Enroll With 3D'),
+              ),
+            ])),
       ),
     );
   }
